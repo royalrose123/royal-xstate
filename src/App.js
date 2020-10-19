1bcfa84e379a699bde92f6f3eb31d64fe3003ed2
Royal Chen <royal.chen@cloud-interactive.com>
2020-10-19 11:17:32 +0800

    Update globalStyle with emotion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Global } from '@emotion/core'
+import globalStyle from 'styles/reboot'
 
 function App() {
-  return <div className='App' />
+  return (
+    <>
+      <Global styles={globalStyle} />
+      <div className='App'>
+        <div>app</div>
+      </div>
+    </>
+  )
 }
 
 export default App
